3c22791fd4622c537e8cbf68b4280000f20960f3
Victoria Lappas <victorialappas@Victorias-MacBook-Pro.local>
2017-11-08 14:15:08 -0700

    Initial commit

---
+++ src/UserWindow.java
@@ -0,0 +1,17 @@
+/*
+ * UserWindow is the class that provides a GUI for the user to interact with.
+ * @author Victoria Lappas
+ */
+import javax.swing.*;
+
+
+public class UserWindow {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
