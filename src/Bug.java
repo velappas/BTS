4ee3a99a8faae637192eb60594182fdb3ed90c34
Evan Hampton <evan.hampton@ucalgary.ca>
2017-11-13 13:26:10 -0700

    Filled out this class. I added the getters/setters that I thought would be necessary

--- src/Bug.java
+++ src/Bug.java
@@ -1,5 +1,68 @@
+//class Bug
+//An entity class to store information about a bug in a product
+//@author Evan Hampton
+
+import java.util.Date;
 
 public class Bug 
 {
-	//TODO: Make this class
+	private int bugID;
+	private String bug_description;
+	private int productID;
+	private String status;
+	private Date date_submitted;
+	private int assigned_developer;
+	private static int IDCount;
+	
+	//Constructor of a Bug
+	//@param description - Description of a bug in a product
+	//@param prodID - ID of the product that has the bug
+	public Bug(String description, int prodID)
+	{
+		bugID = IDCount++;
+		bug_description = description;
+		productID = prodID;
+		status = "Submitted";	//Default status of a new bug
+		date_submitted = new Date();	//This constructor allocates a new date object initialized to the time it is allocated.
+		assigned_developer = -1;	//Set the developer ID to be a non-existent ID so that you can tell if the bug is assigned yet
+	}
+	
+	//Can set the status of 'this' Bug
+	//@param newStat - the new status to be set
+	public void setStatus(String newStat)
+	{
+		status = newStat;
+	}
+	
+	//Check to see if 'this' bug is currently assigned to a developer
+	//@return false if bug not assigned, true if it is
+	public boolean isAssigned()
+	{
+		return assigned_developer < 0 ? false : true;
+	}
+	
+	//Set the working developer of this bug
+	//@param devID - ID of the new current working developer
+	public void setDeveloper(int devID)
+	{
+		assigned_developer = devID;
+	}
+	
+	//@return - ID of 'this' bug
+	public int getID()
+	{
+		return bugID;
+	}
+	
+	//@return - Description of 'this' bug
+	public String getDescription()
+	{
+		return bug_description;
+	}
+	
+	//@return - the ID of the product that 'this' bug is associated with
+	public int getProduct()
+	{
+		return productID;
+	}
 }
