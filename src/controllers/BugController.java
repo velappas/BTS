13e3b43fba5eb22e1d38eb515e9dcd7b83bed412
Evan Hampton <evan.hampton@ucalgary.ca>
2017-11-13 20:27:40 -0700

    Pretty much finished up this class
    Finished up this class with all of the methods we had in the design
    stage, except for generateBugReport.

--- src/controllers/BugController.java
+++ src/controllers/BugController.java
@@ -32,6 +32,7 @@ public class BugController
 		fw.flush();
 		fw.close();
 	}
+	
 	//Removes the lines in the text file associated with bug_ID
 	//Will delete multiple lines if there are duplicate bug_ID's
 	//@param bug_ID - ID of the bug to delete from the text file
@@ -97,28 +98,168 @@ public class BugController
 		fr.close();
 		return toReturn;
 	}
-//	+browseProductBugs(int product_ID): Bug [ ] 
-//	+browseAssignedBugs(int developer_id): Bug []
-//	+assignDeveloper(int bug_id, int developer_id): void
-//	+updateBugState(String status): void
-//	+generateBugReport(Bug [ ] all_bugs): File 
 	
-	public static void main(String [] args)
+	//Browse bugs pertaining to a specific product
+	//@param product_ID - The return value is the list of bugs associated with the product with return value product_ID
+	//@return - null if no bugs found, or a Vector containing all of the bugs found associated with the specified ID
+	//@throws FileNotFoundException - if the database pathname could not be found
+	//@throws IOException - if an IO error occurs while reading from the database
+	public Vector<Bug> browseProductBugs(int product_ID) throws FileNotFoundException, IOException
 	{
-		BugController test = new BugController();
-		try
+		Vector<Bug> toReturn = new Vector<Bug>();
+		BufferedReader fr = new BufferedReader(new FileReader("CurrentBugs.txt"));
+		
+		while(true)
 		{
-			test.submitBug("test1", 1);
-			test.submitBug("test2", 212);
-			test.submitBug("test3", 132321);
-			Vector<Bug> vec = test.browseAllBugs();
-			for(int i = 0; i < vec.size(); i++)
-				System.out.println(vec.get(i));
+			String temp = fr.readLine();
+			
+			if(temp == null)
+				break;
+			
+			if(Integer.parseInt(temp.split(", ")[2]) == product_ID)	//Only add the bug to the return value if the product ID matches
+			{
+				SimpleDateFormat date = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");	//Dates in the database are formatted like this
+				String arr[] = temp.split(", ");	//Split the vector by spaces and commas, as that's how they are organized in the database
+				toReturn.add(new Bug(Integer.parseInt(arr[0]), arr[1], Integer.parseInt(arr[2]), arr[3], date.parse(arr[4], new ParsePosition(0)), Integer.parseInt(arr[5])));
+			}
 		}
-		catch(Exception a)
-		{
-			System.out.println("fsdafdsa");
-			a.printStackTrace();
-		}
+		
+		fr.close();
+		
+		if(toReturn.size() == 0)
+			return null;
+		else
+			return toReturn;
 	}
+	
+	//Browse bugs assigned to a specific developer
+	//@param developer_id - The return value is the list of bugs assigned to the developer with ID == developer_id
+	//@return - null if no bugs found, or a Vector containing all of the bugs assigned to the developer with developer ID equal to developer_id
+	//@throws FileNotFoundException - if the database pathname could not be found
+	//@throws IOException - if an IO error occurs while reading from the database
+	Vector<Bug> browseAssignedBugs(int developer_id) throws FileNotFoundException, IOException
+	{
+		Vector<Bug> toReturn = new Vector<Bug>();
+		BufferedReader fr = new BufferedReader(new FileReader("CurrentBugs.txt"));
+		
+		while(true)
+		{
+			String temp = fr.readLine();
+			
+			if(temp == null)
+				break;
+			
+			if(Integer.parseInt(temp.split(", ")[5]) == developer_id)	//Only add the bug to the return value if the developer ID matches
+			{
+				SimpleDateFormat date = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");	//Dates in the database are formatted like this
+				String arr[] = temp.split(", ");	//Split the vector by spaces and commas, as that's how they are organized in the database
+				toReturn.add(new Bug(Integer.parseInt(arr[0]), arr[1], Integer.parseInt(arr[2]), arr[3], date.parse(arr[4], new ParsePosition(0)), Integer.parseInt(arr[5])));
+			}
+		}
+		
+		fr.close();
+		
+		if(toReturn.size() == 0)
+			return null;
+		else
+			return toReturn;
+	}
+	
+	//Sets the assigned_developer field of a bug in the database
+	//Will assign duplicate bugs, although there shouldn't be any in the database
+	//@param bug_id - the ID of the bug to change
+	//@param developer_id - the ID of the developer that will work on this bug
+	//@return - false if bug with ID bug_id not found in database, otherwise true
+	//@throws FileNotFoundException - if the database pathname could not be found
+	//@throws IOException - if an IO error occurs while reading from the database
+	boolean assignDeveloper(int bug_id, int developer_id) throws FileNotFoundException, IOException
+	{
+		boolean found = false;
+		File currentBugs = new File("CurrentBugs.txt");
+		BufferedReader fr = new BufferedReader(new FileReader(currentBugs));
+		Vector<String> lines = new Vector<String>();
+		
+		while(true)
+		{
+			String tString = fr.readLine();	//Read a line from the database
+			
+			if(tString == null)
+				break;
+			
+			if(Integer.parseInt(tString.split(", ")[0]) == bug_id)	//Check if the ID in the database matches the input
+			{
+				found = true;
+				SimpleDateFormat date = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");	//Dates in the database are formatted like this
+				String arr[] = tString.split(", ");	//Split the vector by spaces and commas, as that's how they are organized in the database
+				Bug temp = new Bug(Integer.parseInt(arr[0]), arr[1], Integer.parseInt(arr[2]), arr[3], date.parse(arr[4], new ParsePosition(0)), developer_id);
+				lines.add(temp.toString());	//Add the edited bug. Everything will be the same except for the changed developer ID field
+			}
+			else
+				lines.add(tString);
+		}
+		fr.close();			
+		
+		BufferedWriter fw = new BufferedWriter(new FileWriter(currentBugs, false));	//Make sure to write at the very beginning of the file.
+		
+		for(int i = 0; i < lines.size(); i++)	//Write all of the stored lines.
+		{
+			fw.write(lines.elementAt(i));
+			fw.newLine();
+			fw.flush();
+		}
+
+		fw.close();
+		
+		return found;
+	}
+	
+	//Updates the status of a specific bug in the database
+	//@param bugID - ID of the bug to modify
+	//@param status - new status of the bug
+	//@return - false if bug with ID bugID not found, otherwise true
+	//@throws FileNotFoundException - if the database pathname could not be found
+	//@throws IOException - if an IO error occurs while reading from the database
+	public boolean updateBugState(int bugID, String status) throws FileNotFoundException, IOException
+	{
+		boolean found = false;
+		File currentBugs = new File("CurrentBugs.txt");
+		BufferedReader fr = new BufferedReader(new FileReader(currentBugs));
+		Vector<String> lines = new Vector<String>();
+		
+		while(true)
+		{
+			String tString = fr.readLine();	//Read a line from the database
+			
+			if(tString == null)
+				break;
+			
+			if(Integer.parseInt(tString.split(", ")[0]) == bugID)	//Check if the ID in the database matches the input
+			{
+				found = true;
+				SimpleDateFormat date = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");	//Dates in the database are formatted like this
+				String arr[] = tString.split(", ");	//Split the vector by spaces and commas, as that's how they are organized in the database
+				Bug temp = new Bug(Integer.parseInt(arr[0]), arr[1], Integer.parseInt(arr[2]), status, date.parse(arr[4], new ParsePosition(0)), Integer.parseInt(arr[5]));
+				lines.add(temp.toString());	//Add the edited bug. Everything will be the same except for the changed developer ID field
+			}
+			else
+				lines.add(tString);
+		}
+		fr.close();			
+		
+		BufferedWriter fw = new BufferedWriter(new FileWriter(currentBugs, false));	//Make sure to write at the very beginning of the file.
+		
+		for(int i = 0; i < lines.size(); i++)	//Write all of the stored lines.
+		{
+			fw.write(lines.elementAt(i));
+			fw.newLine();
+			fw.flush();
+		}
+
+		fw.close();
+		
+		return found;
+	}
+	
+	//public File generateBugReport(Bug [] all_bugs)
+	//TODO: Finish this method
 }
